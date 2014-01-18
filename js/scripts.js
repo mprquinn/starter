d6658dfc4d732c2946761d1042d3b32404b8d92a
Mike Quinn <mprquinn@gmail.com>
2014-01-18 16:11:52 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/*------------------------------
+
+	General Scripts
+	for site interactions
+
+------------------------------*/
+
+var SizeHeader = (function ($){
+	var s;
+
+	return {
+
+		settings: {
+
+		},
+
+		init: function (){
+			s = this.settings;
+			console.log('running header resize');
+		}
+	}
+
+})(jQuery);
